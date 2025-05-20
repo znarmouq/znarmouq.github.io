ae5d16b16379b64000d44c8de1bc467f37d5e5e7
znarmouq <znarmouq@gmail.com>
2025-05-19 21:15:26 -0400

    fixed static issues

--- portfolio/next.config.ts
+++ portfolio/next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   /* config options here */
+  output: "export",
+  // basePath: '/portfolio',
+  // assetPrefix: '/znarmouq.github.io/',
+  distDir: 'dist',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
