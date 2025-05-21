d9c4cdf6ae0d68f737293bef424e87ddd8c666a6
znarmouq <znarmouq@gmail.com>
2025-05-20 23:44:59 -0400

    added stats page

--- portfolio/src/app/fish/page.tsx
+++ portfolio/src/app/fish/page.tsx
@@ -1,15 +1,48 @@
+// app/fish/page.tsx
 "use client";
 import Image from "next/image";
-import React, { PureComponent } from 'react';
 import { Radar, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis, ResponsiveContainer } from 'recharts';
-import Link from "next/link";
 
-export default function Home() {
+
+
+  const fishCaught = [
+    { name: "Carp", count: 4 },
+    { name: "Bass", count: 1 },
+    { name: "Crappie", count: 4 },
+    { name: "Snake Head", count: 0 },
+    { name: "Trout", count: 0 },
+  ];
+
+export default function Fish() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-zilla-slab)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
       
-        Portfolio. Coming Soon.
+        {/* Fishing Stats */}
+
+        {/* <h1>Fish I Caught</h1>
+        <FishPage /> */}
+
+    <div className="flex flex-col items-center">
+        Fishing Stats
+        <div style={{ width: 500, height: 500 }}>
+          <ResponsiveContainer width="100%" height="100%">
+            <RadarChart cx="50%" cy="50%" outerRadius="80%" data={fishCaught}>
+              <PolarGrid />
+              <PolarAngleAxis dataKey="name" />
+              {/* <PolarRadiusAxis /> */}
+              <Radar
+                name="Zaid"
+                dataKey="count"
+                stroke="#8884FF"
+                fill="#88ff"
+                fillOpacity={0.6}
+              />
+            </RadarChart>
+          </ResponsiveContainer>
+        </div>
+    </div>
+
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
@@ -60,16 +93,17 @@ export default function Home() {
 
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="/fish"
+          href="/"
         >
           <Image
             aria-hidden
-            src="/fish.svg"
+            src="/home.svg"
             alt="Fish Stats"
             width={16}
             height={16}
           />
-          Stats
+
+          Home
         </a>
         
       </footer>
